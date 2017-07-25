81021d24627e0095b524e875bf566a30456d063b
Mohsen Koohi <koohi@burge.ir>
2017-07-25 07:49:47 +0430

    product garllery  correction

--- ATS/CMF/Web/application/views/en/customer/product.php
+++ ATS/CMF/Web/application/views/en/customer/product.php
@@ -54,54 +54,57 @@
 				<?php echo $product_info['pc_content'] ?>
 			</div>
 		</div>
-		<div class="row post-gallery">
-			<?php 
-				if($product_gallery)
+		<?php if($product_gallery){ ?>
+			<div class="row post-gallery">
+				<?php 
 					foreach($product_gallery as $img)
 					{ 
-			?>
-				<div class="four columns img-div" title="<?php echo $img['text'];?>"  href="{product_gallery_url}/<?php echo $img['image'];?>" >
-					<div class="img lazy-load"  data-ll-url="{product_gallery_url}/<?php echo $img['image'];?>"
-					 data-ll-type="background-image" >
-					</div>
-					<div class="text">
-						<?php echo $img['text'];?>
-					</div>
-				</div>
-			<?php } ?>
+				?>
+						<div class="four columns img-div" title="<?php echo $img['text'];?>"  href="{product_gallery_url}/<?php echo $img['image'];?>" >
+							<div class="img lazy-load"  data-ll-url="{product_gallery_url}/<?php echo $img['image'];?>"
+							 data-ll-type="background-image" >
+							</div>
+							<div class="text">
+								<?php echo $img['text'];?>
+							</div>
+						</div>
+				<?php 
+					} 
+				?>
 
-			<script type="text/javascript">
+				<script type="text/javascript">
 
-				$(window).load(function()
-				{
-					$("body").addClass("product-page");
-					$(window).on("resize",setColorBox);
-					setColorBox();
-				});
+					$(window).load(function()
+					{
+						$("body").addClass("product-page");
+						$(window).on("resize",setColorBox);
+						setColorBox();
+					});
 
-				function setColorBox()
-				{
-					$.colorbox.remove();
-					$(".img-div").unbind("click");
+					function setColorBox()
+					{
+						$.colorbox.remove();
+						$(".img-div").unbind("click");
 
-					if($(window).width() < 600)
-						$(".img-div").click(function(event)
-						{
-							window.open($(event.target).parent().attr("href"));
-						});
-					else
-						$(".img-div").colorbox({
-							rel:"group"
-							,iframe:false
-							,width:"80%"
-							,height:"80%"
-							,opacity:.4
-							,fixed:true
-							,current:"{image_text} {current} {from_text} {total}" 
+						if($(window).width() < 600)
+							$(".img-div").click(function(event)
+							{
+								window.open($(event.target).parent().attr("href"));
+							});
+						else
+							$(".img-div").colorbox({
+								rel:"group"
+								,iframe:false
+								,width:"80%"
+								,height:"80%"
+								,opacity:.4
+								,fixed:true
+								,current:"{image_text} {current} {from_text} {total}" 
 
-						});
-				}
-			</script>
-		</div>
+							});
+					}
+				</script>
+			</div>
+		<?php } ?>
 	</div>
 </div>
